8b65264562e40567a07b0faa84320de47167fcc6
Yaser Bahrami <yaser.bahrami@gmail.com>
2023-03-02 00:56:10 +0330

    add diff

--- resources/views/newsagency.blade.php
+++ resources/views/newsagency.blade.php
@@ -26,7 +26,7 @@
                                     <!-- <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>-->
                                 </div>
                                 <div class="card-footer">
-                                    <small class="text-muted">{{$item->diff()}} قبل</small>
+                                    <small class="text-muted">{{$news_item->diff()}} قبل</small>
                                     <a href="/newsagency/{{$news_item->source->newsAgancy->newsagency}}">  <small class="text-muted">{{$news_item->source->newsAgancy->newsagency}}  </small> </a>
 
                                 </div>
