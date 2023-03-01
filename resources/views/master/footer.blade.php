c04b3cefa719b81e62fa24f77bbd8c16d4ab0128
Yaser Bahrami <yaser.bahrami@gmail.com>
2023-03-02 00:05:11 +0330

    add Tag H1

--- resources/views/master/footer.blade.php
+++ resources/views/master/footer.blade.php
@@ -1,7 +1,7 @@
 <footer class="row" dir="rtl">
     <section class="col-12 col-sm-12 col-md-8 col-lg-8 col-xl-6">
       <img class="img-fluid" src="{{asset('images/logo_footer.png')}}" />
-      <p><a href="/">باماخبر </a>یک موتور جستجوگر اینترنتی است و هیچگونه مسئولیتی در قبال محتویات خبرها ندارد</p>
+      <h1><a href="/">باماخبر </a>یک موتور جستجوگر اینترنتی است و هیچگونه مسئولیتی در قبال محتویات خبرها ندارد</h1>
     </section>
     <section class="col-12 col-sm-12 col-md-4 col-lg-4 col-xl-6">
       <p>طراحی و پیاده سازی توسط <a href="tel:+989376578529">یاسر بهرامی</a></p>
