74d17b6eddfce67cce2c1f31fe6c8a86debe205b
Yaser Bahrami <yaser.bahrami@gmail.com>
2023-05-29 03:09:07 +0430

    footer

--- resources/views/master/footer_home.blade.php
+++ resources/views/master/footer_home.blade.php
@@ -1,7 +1,7 @@
 <footer class="row" dir="rtl">
     <section class="col-12 col-sm-12 col-md-8 col-lg-8 col-xl-6">
       <img class="img-fluid" src="{{asset('images/logo_footer.png')}}" />
-      <h1><a href="/">باماخبر </a>یک موتور جستجوگر اینترنتی است و هیچگونه مسئولیتی در قبال محتویات خبرها ندارد</h1>
+      <h1><a href="/">باماخبر </a>یک موتور هوشمند اینترنتی است و هیچگونه مسئولیتی در قبال محتویات خبرها ندارد</h1>
 
     </section>
     <section class="col-12 col-sm-12 col-md-4 col-lg-4 col-xl-6">
